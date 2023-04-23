b4a3987402b40cf02acea71d35b4643844322e42
Natriumm <gabrielengames1999@gmail.com>
2023-04-23 08:17:44 +0200

    Bug Fixing round 1

--- indexScript.js
+++ indexScript.js
@@ -3,11 +3,13 @@ var bufferValue = 0;
 var symbolBuffer = 0;
 var buffer = 0;
 var mathValue = 0;
+var decimal = 0;
 function clearValues() {
     mathValue = 0;
     bufferValue = 0;
     symbolBuffer = 0;
     buffer = 0;
+    decimal = 0;
     result.innerHTML = mathValue;
 }
 function removeNumber() {
@@ -16,17 +18,24 @@ function removeNumber() {
     }
     else {
         mathValue = mathValue.substring(0, mathValue.length - 1);
+        if (mathValue.substring(mathValue.length - 1, mathValue.length) == ".") {
+            mathValue = mathValue.substring(0, mathValue.length - 1);
+            decimal = 0;
+        }
     }
 }
 function handleNumber(value) {
     if (mathValue == 0) {
+        decimal = 0;
         mathValue = value;
         result.innerHTML = mathValue;
+        return;
     }
-    else {
-        mathValue = mathValue + value;
-        result.innerHTML = mathValue;
+    if (value == "." && decimal == 1) {
+        return;
     }
+    mathValue = mathValue + value;
+    result.innerHTML = mathValue;
 }
 
 function addition() {
@@ -86,21 +95,24 @@ function splitting() {
 }
 function executeBuffer() {
     switch (symbolBuffer) {
+        case 0: {
+            if (mathValue != 0) {
+                bufferValue = mathValue;
+            }
+            mathValue = 0;
+        } return;
         case 1: {
-            mathValue = parseInt(bufferValue) + parseInt(mathValue);
+            mathValue = parseFloat(bufferValue) + parseFloat(mathValue);
         } break;
         case 2: {
-            mathValue = parseInt(bufferValue) - parseInt(mathValue);
+            mathValue = parseFloat(bufferValue) - parseFloat(mathValue);
         } break;
         case 3: {
-            mathValue = parseInt(bufferValue) * parseInt(mathValue);
+            mathValue = parseFloat(bufferValue) * parseFloat(mathValue);
         } break;
         case 4: {
-            mathValue = parseInt(bufferValue) / parseInt(mathValue);
+            mathValue = parseFloat(bufferValue) / parseFloat(mathValue);
         } break;
-        case 5: {
-            clearValues()
-        } return;
     }
     result.innerHTML = mathValue;
     bufferValue = mathValue;
